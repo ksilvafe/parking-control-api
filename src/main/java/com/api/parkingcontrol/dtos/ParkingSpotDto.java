e9b3c88ab2e7b980a10571d49f8d33218ba62d17
ksilvafe <ferreirakevin325@gmail.com>
2022-05-08 05:02:40 -0300

    use @getter and @setter

--- src/main/java/com/api/parkingcontrol/dtos/ParkingSpotDto.java
+++ src/main/java/com/api/parkingcontrol/dtos/ParkingSpotDto.java
@@ -7,6 +7,7 @@ import lombok.Setter;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
+@Getter @Setter
 public class ParkingSpotDto {
 
     @NotBlank
@@ -26,68 +27,4 @@ public class ParkingSpotDto {
     private String apartment;
     @NotBlank
     private String block;
-
-    public String getParkingSpotNumber() {
-        return parkingSpotNumber;
-    }
-
-    public void setParkingSpotNumber(String parkingSpotNumber) {
-        this.parkingSpotNumber = parkingSpotNumber;
-    }
-
-    public String getLincensePlateCar() {
-        return lincensePlateCar;
-    }
-
-    public void setLincensePlateCar(String lincensePlateCar) {
-        this.lincensePlateCar = lincensePlateCar;
-    }
-
-    public String getBrandCar() {
-        return brandCar;
-    }
-
-    public void setBrandCar(String brandCar) {
-        this.brandCar = brandCar;
-    }
-
-    public String getModelCar() {
-        return modelCar;
-    }
-
-    public void setModelCar(String modelCar) {
-        this.modelCar = modelCar;
-    }
-
-    public String getColorCar() {
-        return colorCar;
-    }
-
-    public void setColorCar(String colorCar) {
-        this.colorCar = colorCar;
-    }
-
-    public String getResponsibleName() {
-        return responsibleName;
-    }
-
-    public void setResponsibleName(String responsibleName) {
-        this.responsibleName = responsibleName;
-    }
-
-    public String getApartment() {
-        return apartment;
-    }
-
-    public void setApartment(String apartment) {
-        this.apartment = apartment;
-    }
-
-    public String getBlock() {
-        return block;
-    }
-
-    public void setBlock(String block) {
-        this.block = block;
-    }
 }
